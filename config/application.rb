ffcddef8cbb6c233eea0dcee94075abc29827b4a
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2016-01-08 10:37:47 -0200

    config
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile=false
+#config.assets.initialize_on_precompile=false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +24,6 @@ module RbInternetSystem
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile=false
   end
 end
